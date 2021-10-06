7fe2686405d781e9032ae9a2ffd350f8c0574f98
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-07 03:39:19 +0530

    Initial commit

---
+++ main.sh
@@ -0,0 +1,7 @@
+- git checkout xyz =
+                checks out the branch, switches to the branch.
+- git checkout -b <branch_name> =
+                creates a new branch and switches to it.
+- git merge <branch_name> =
+                this command takes changes from the given branch,
+                and merges with the current branches we are on. 
